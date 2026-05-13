d541fe16400b3b4d4fab90e5a08ae8185c5346d7
Trần Đình Nam <trandinhnamuet@gmail.com>
2026-05-13 21:35:57 +0700

    setup to host in AZDIGI

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost', port: '3001', pathname: '/uploads/**' },
       { protocol: 'https', hostname: 'github.com', pathname: '/user-attachments/assets/**' },
+      { protocol: 'https', hostname: 'api.lonfantafc.com', pathname: '/uploads/**' },
     ],
   },
 };
